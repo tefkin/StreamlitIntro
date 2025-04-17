ff916a59d3a98dbeb85a9738078b091523d34444
Richard Allen <richard.t.allen@outlook.com>
2025-04-17 19:09:23 +0000

    added a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,14 @@ st.title("🎈 My new streamlit app for Richard Updated two")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+# streamlit assumes the df should be writen to screen so allows you to skip
+# the write command - magic
+df
