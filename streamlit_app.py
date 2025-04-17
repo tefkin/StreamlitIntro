0822ed0e375df115ef5f12038c5d1f9f41d42ee4
Richard Allen <richard.t.allen@outlook.com>
2025-04-17 18:11:00 +0000

    title change two

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new streamlit app for Richard Updated")
+st.title("🎈 My new streamlit app for Richard Updated two")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
